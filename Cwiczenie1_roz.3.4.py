95bf0681cd678848a384466d659a79cda32d1eb7
RafalTaraska <paulo.dorosz@gmail.com>
2022-10-25 23:31:15 +0200

    calosc

--- Cwiczenie1_roz.3.4.py
+++ Cwiczenie1_roz.3.4.py
@@ -1,3 +1,7 @@
+from ast import Del, Delete
+from unicodedata import numeric
+
+
 numbers = [5,32,56,2,2,16,7,10,23,100]
 mean_number = 0
 
@@ -12,7 +16,16 @@ for seq, number in enumerate(numbers):
         number = number - x
         numbers[seq] = number
 
+#print(numbers)
+numbers.remove(min(numbers))
+numbers.remove(max(numbers))
 print(numbers)
+
+mean_number = sum(numbers) / len(numbers)
+print(mean_number)
+
+
+
 
    
 
